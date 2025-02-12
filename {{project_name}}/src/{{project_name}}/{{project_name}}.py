4f05c481bfaeb7c8ce5fc3df8502e6efaa6180d6
edalfon <edalfon@gmail.com>
2025-02-12 08:15:00 +0100

    docs

--- {{project_name}}/src/{{project_name}}/{{project_name}}.py
+++ {{project_name}}/src/{{project_name}}/{{project_name}}.py
@@ -1,2 +1,29 @@
+"""
+This module initializes the {{project_name}} package and contains a simple function that returns a greeting message.
+    hello(): Returns a greeting message "Hello World".
+This module initializes the {{project_name}} package.
+
+The __init__.py file is used to mark a directory as a Python package and can
+also be used to execute initialization code for the package.
+
+Attributes:
+  None
+
+Functions:
+  None
+"""
+
+
+
 def hello():
+    """
+    This module contains a simple function that returns a greeting message.
+    """
+
+        """
+        Returns a greeting message.
+
+        Returns:
+            str: A greeting message "Hello World".
+        """
     return "Hello World"
