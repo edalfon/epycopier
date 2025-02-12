d67e58a2be46b2086197d48eb9fd879086071a7a
edalfon <edalfon@gmail.com>
2025-02-12 13:23:43 +0100

    mkdocs

--- {{project_name}}/src/{{project_name}}/{{project_name}}.py
+++ {{project_name}}/src/{{project_name}}/{{project_name}}.py
@@ -8,8 +8,6 @@ Examples:
     >>> {{ project_name }}.hello()
 """
 
-from typing import Union
-
 
 def hello():
     """Says Hello World.
@@ -21,23 +19,3 @@ def hello():
 
     """
     return "Hello World"
-
-
-def add(a: Union[float, int], b: Union[float, int]) -> float:
-    """Calculates the sum of two numbers.
-
-    Examples:
-        >>> add(4.0, 2.0)
-        6.0
-        >>> add(4, 2)
-        6.0
-
-    Args:
-        a: first number
-        b: second number
-
-    Returns:
-        sum of the first and the second number
-    """
-
-    return float(a + b)
